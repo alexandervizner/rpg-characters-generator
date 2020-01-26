a681657a1c12ed570600b1a18ef0ba25d167408a
Alex Vizner <alexandr.vizner@gmail.com>
2020-01-26 17:06:28 +0200

    Add missed initialization and call in App

--- src/main/java/rpg/characters/generator/App.java
+++ src/main/java/rpg/characters/generator/App.java
@@ -1,6 +1,15 @@
 package rpg.characters.generator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        List<Die> diceSet = Arrays.asList(new Die(), new Die(), new Die(), new Die());
+        DiceRoller roller = new DiceRoller(diceSet);
+        List<Integer> attrs = roller.generate();
+        Character elf = new Character("Elf", attrs);
+
+        System.out.println(elf);
     }
 }
